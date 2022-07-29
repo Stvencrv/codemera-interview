cd1babc7edbe56f08fbc0b740fa3377260f24069
Huascar Mejia <mejiahuascar@gmail.com>
2022-07-29 16:30:10 -0400

    Wrapper added and provider initialized

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './styles/App.css';
+import { useReducer } from "react";
+import TodosDispatch from "./providers/todoProvider";
+import TodosWrapper from "./wrappers/todoWrapper";
+import todosReducer, { initialState } from "./reducers/todosReducer";
+
+import "antd/dist/antd.css";
+import "./styles.css";
 
 function App() {
+  const [state, dispatch] = useReducer(todosReducer, initialState);
+
+  console.log(state);
+
   return (
     <div className="App">
-      
+      <h1>Todo App With UseReducer</h1>
+      <TodosDispatch.Provider value={dispatch}>
+        <TodosWrapper todos={state.todos} currentView={state.currentView} />
+      </TodosDispatch.Provider>
     </div>
   );
 }
---
+++ src/wrappers/todoWrapper.js
@@ -0,0 +1,52 @@
+import { useContext } from 'react';
+import { List, Tabs } from 'antd';
+import TodoItem from "./../components/Todo/TodoItem";
+import TodoForm from "./../components/Todo/TodoForm";
+import TodosDispatch from "../../providers/todoProvider";
+import { changeView } from "../actions/todoActions";
+
+const TabPane = Tabs.TabPane;
+
+export default function TodosWrapper(props) {
+  const dispatch = useContext(TodosDispatch);
+  const { todos, currentView } = props;
+
+  const handleTabChange = (value) => {
+    dispatch(changeView(value));
+  };
+
+  const filterTodos = (type) => {
+    switch (type) {
+      case "done":
+        return todos.filter((todo) => todo.isDone);
+      case "pending":
+        return todos.filter((todo) => !todo.isDone);
+      default:
+        return todos;
+    }
+  };
+
+  return (
+      <>
+        <TodoForm />
+        <Tabs defaultActiveKey="all" onChange={handleTabChange}>
+            <TabPane tab={`All (${filterTodos("all").length})`} key="all" />
+            <TabPane tab={`Done (${filterTodos("done").length})`} key="done" />
+            <TabPane
+            tab={`Pending (${filterTodos("pending").length})`}
+            key="pending"
+            />
+        </Tabs>
+        <List
+            className="ul-todos"
+            itemLayout="horizontal"
+            bordered={true}
+            dataSource={filterTodos(currentView)}
+            renderItem={(todo, index) => (
+            <TodoItem todo={todo} id={index} key={`todo_${index}`} />
+            )}
+            locale={{ emptyText: "No to-dos found." }}
+        />
+    </>
+  );
+}
